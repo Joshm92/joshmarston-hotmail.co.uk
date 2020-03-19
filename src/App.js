78154ee594f69601773a4744a7fa5d87f8824aa1
Josh <joshmarston@hotmail.co.uk>
2020-03-19 10:39:40 +0000

    split the score and scores up into individual components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Winner from "./Components/Winner.js"
 import Header from "./Components/Header.js"
 import Reset from "./Components/Reset.js"
+import Scores from "./Components/Scores.js"
 
 
 
@@ -12,7 +13,9 @@ const App = ({player1Score,
   reset, 
   player1Serving,
   winner,}) => (
+
     <React.Fragment>
+
         {/* header */}
         {/* <header className="jumbotron mt-4 mb-0">
             <h1>PongPing</h1>
@@ -21,13 +24,23 @@ const App = ({player1Score,
         <Header />
 
         {/* scores */}
-        <div className="row mb-4">
+
+        < Scores 
+            player1Serving={ player1Serving }
+            player1Score={ player1Score }
+            player2Score={ player2Score }
+            handlePlayer1Score={handlePlayer1Score}
+            handlePlayer2Score={handlePlayer2Score}
+        />
+
+        {/* <div className="row mb-4">
             <div className="col-md-6 mt-4">
                 <div className={"card text-center" + (player1Serving ? " bg-dark text-white" : "")}>
                     <h5 className="card-header">Player 1</h5>
                     <div className="card-body">
                         <p className="card-text display-1">{player1Score}</p>
                     </div>
+
                     <div className="card-footer">
                         <button onClick={ handlePlayer1Score} 
                         className="form-control btn btn-success">+</button>
@@ -47,7 +60,7 @@ const App = ({player1Score,
                     </div>
                 </div>
             </div>
-        </div>
+        </div> */}
 
         { /* winner message */}
         {/* { winner > 0 ? (<h2 className="alert alert-success">Player { winner } wins!</h2>) : null } */}
